d5fc0983e213110d85f9d3edbe788cb9bd43a1f9
burrasathish <sathish.burra@mountblue.tech>
2020-02-26 15:15:38 +0530

    ipl questions with queries

--- src/pacakge/ipl/main.java
+++ src/pacakge/ipl/main.java
@@ -1,24 +1,41 @@
     package pacakge.ipl;
     import java.io.IOException;
+    import java.util.Scanner;
+
 
     public class main {
     public static  void main(String args[]) throws IOException {
 
-        util k=new util();
-        k.fetchmatches();
-        k.fetchdeliveries();
-        Economyy Ec=new Economyy();
-         Ec.EconomyBowler();
 
-//        totalMatchesplayed yeartotal=new totalMatchesplayed();
-//        yeartotal.getyeardata();
+        System.out.println("1. Number of matches played per year of all the years in IPL.");
+        System.out.println("2. Number of matches won of all teams over all the years of IPL.");
+        System.out.println("3. For the year 2016 get the extra runs conceded per team.");
+        System.out.println("4. For the year 2015 get the top economical bowlers.");
 
-//        matcheswon mwon=new matcheswon();
-//        mwon.getteamdata();
 
-//
-//        teamExtraruns extraruns=new teamExtraruns();
-//        extraruns.getteam();
+        Scanner sc = new Scanner(System.in);
+        int k=sc.nextInt();
+
+        switch (k) {
+            case 1:
+
+       totalMatchesplayed yeartotal=new totalMatchesplayed();
+       yeartotal.getyeardata();
+                break;
+            case 2:
+                matcheswon mwon=new matcheswon();
+                mwon.getteamdata();
+
+                break;
+            case 3:
+                teamExtraruns extraruns=new teamExtraruns();
+                extraruns.getteam();
+                break;
+            case 4:
+                Economyy Ec=new Economyy();
+                Ec.EconomyBowler();
+                break;
+        }
 
 }
 }
